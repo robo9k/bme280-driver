2b02bce413ce490a9e6506396263bd2bbd92ee26
robot9001 <robo9k@symlink.io>
2025-04-25 22:02:20 +0200

    Add initial empty crate

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(any(test, feature = "std")), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg_hide))]
+#![cfg_attr(docsrs, doc(cfg_hide(docsrs)))]
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
